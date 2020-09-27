e887abe668e92a3d06175dc3d1c162b5cd13b5ff
praketa <praketasxena@gmail.com>
2020-09-27 14:12:12 -0500

    python transformation check

--- index.py
+++ index.py
@@ -5,7 +5,23 @@ projPath=r"D:\python\newsFrontSwarajya\data"
 file = projPath+r"\news.csv"
 outfile="out.json"
 data = pd.read_csv(file)
-headline = data.loc[:0,'headline']
-link = data.loc[:0,'links']
 
-data.to_json(projPath+outfile,orient="records")
+compressed = data.loc[:,['Paper_Name','headline',"links"]]
+
+outData=dict.fromkeys(compressed['Paper_Name'].unique(),[])
+#print(compressed.to_dict('list'))
+# data.to_json(projPath+outfile,orient="records")
+
+l=[]
+
+for index, row in compressed.iterrows():
+    if row["Paper_Name"] in outData:
+        outData[row["Paper_Name"]].append({"headline":row['headline'],"links":row['links']})
+    else:
+        pass
+
+        #l.append({"headline":row['headline'],"links":row['links']})
+
+print(outData['The_HT'])
+
+#print(l[1])
