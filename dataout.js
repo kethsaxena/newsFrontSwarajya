7d922d9819a39a57e474db78d23e72737adb670c
praketa <praketasxena@gmail.com>
2020-09-27 20:55:10 -0500

    js part

--- dataout.js
+++ dataout.js
@@ -1,151 +1,93 @@
 var news = 
-    [
-
-    {
-        "headline": "FinCEN Files | Antiques smuggler in TN jail, and a trade that flourished even after his arrest",
-        "links": "https:\/\/indianexpress.com\/article\/express-exclusive\/fincen-filesremove-antiques-smuggler-in-tamil-nadu-jail-and-a-trade-that-flourished-even-after-his-arrest-6606764\/",
-        "text": "FinCEN Files | Antiques smuggler in TN jail, and a trade that flourished even after his arrest",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "India, China agree no more escalation, to see if it holds",
-        "links": "https:\/\/indianexpress.com\/article\/india\/india-china-agree-no-more-escalation-to-see-if-it-holds-6606802\/",
-        "text": "India, China agree no more escalation, to see if it holds",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "As Sept surge sweeps rural Maharashtra, hunt for beds",
-        "links": "https:\/\/indianexpress.com\/article\/india\/as-sept-surge-sweeps-rural-maharashtra-hunt-for-beds-6606828\/",
-        "text": "As Sept surge sweeps rural Maharashtra, hunt for beds",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Explained: How remunerative is farming at present?",
-        "links": "https:\/\/indianexpress.com\/article\/explained\/simply-put-the-farmer-a-field-report-6606785\/",
-        "text": "Explained: How remunerative is farming at present?",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Ravi Shankar Prasad: \u00e2\u20ac\u02dcOur commitment to farmers remains, Opposition will be exposed\u00e2\u20ac\u2122",
-        "links": "https:\/\/indianexpress.com\/article\/india\/ravi-shankar-prasad-our-commitment-to-farmers-remains-once-people-realise-it-opposition-will-be-exposed-6606797\/",
-        "text": "Ravi Shankar Prasad: \u00e2\u20ac\u02dcOur commitment to farmers remains, Opposition will be exposed\u00e2\u20ac\u2122",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Cyprus to Isle of Man: Over 100 transactions linked to Max chairman",
-        "links": "https:\/\/indianexpress.com\/article\/express-exclusive\/fincen-files-cyprus-to-isle-of-man-over-100-transactions-linked-to-max-chairman-6606773\/",
-        "text": "Cyprus to Isle of Man: Over 100 transactions linked to Max chairman",
-        "Paper_Name": "Indian_Express",
-        "Story_type": "Main_Story"
-    },
-    {
-        "headline": "Mumbai Rains LIVE: City municipal commissioner asks citizens to remain indoors",
-        "links": "https:\/\/liveupdates.hindustantimes.com\/india\/mumbai-rains-live-updates-western-railway-suspends-services-due-to-waterlogging-21600829580738.html",
-        "text": "Mumbai Rains LIVE: City municipal commissioner asks citizens to remain indoors",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Landslide warning in north Bengal including Darjeeling and Kalimpong",
-        "links": "https:\/\/www.hindustantimes.com\/kolkata\/landslide-warning-in-north-bengal-including-darjeeling-and-kalimpong\/story-hPqeXjra6v1PEYIvwSGfQL.html",
-        "text": "Landslide warning in north Bengal including Darjeeling and Kalimpong",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Would Tendulkar score a ton without spectators? Laxman gives fitting reply",
-        "links": "https:\/\/www.hindustantimes.com\/cricket\/would-sachin-tendulkar-score-a-ton-without-spectators-vvs-laxman-gives-fitting-reply\/story-XEqAH4wmULiKWKxEHYk9zO.html",
-        "text": "Would Tendulkar score a ton without spectators? Laxman gives fitting reply",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "IPL 2020: Dhoni explains why he batted at No. 7 for CSK against RR",
-        "links": "https:\/\/www.hindustantimes.com\/cricket\/ipl-2020-ms-dhoni-explains-the-reason-behind-batting-at-no-7-for-csk-against-rr\/story-FBDhclFh3mHEGjkwt4AhTM.html",
-        "text": "IPL 2020: Dhoni explains why he batted at No. 7 for CSK against RR",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Covid-19 meet with PM: Maharashtra may raise issue of lack of Centre\u00e2\u20ac\u2122s support ",
-        "links": "https:\/\/www.hindustantimes.com\/cities\/covid-19-meet-with-pm-maharashtra-may-raise-issue-of-lack-of-centre-s-support\/story-yNIEj4ktfbdBbWBwaTPWuM.html",
-        "text": "Covid-19 meet with PM: Maharashtra may raise issue of lack of Centre\u00e2\u20ac\u2122s support ",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Parliament monsoon session: What to expect today",
-        "links": "https:\/\/www.hindustantimes.com\/india-news\/parliament-monsoon-session-what-to-expect-today\/story-KpkkKk22G45TZ8uv9y9XjJ.html",
-        "text": "Parliament monsoon session: What to expect today",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "PM Modi to meet chief ministers of  7 states. Here\u00e2\u20ac\u2122s what to expect",
-        "links": "https:\/\/www.hindustantimes.com\/india-news\/pm-modi-to-meet-chief-ministers-of-these-7-states-here-s-why\/story-bEOs2kOg8RCoxtFkn1aD8H.html",
-        "text": "PM Modi to meet chief ministers of  7 states. Here\u00e2\u20ac\u2122s what to expect",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Local trains cancelled in Mumbai due to heavy rainfall and waterlogging",
-        "links": "https:\/\/www.hindustantimes.com\/mumbai-news\/local-trains-cancelled-in-mumbai-due-to-heavy-rainfall-and-waterlogging\/story-KN0BLtSp6R2f6609Fx8EwL.html",
-        "text": "Local trains cancelled in Mumbai due to heavy rainfall and waterlogging",
-        "Paper_Name": "The_HT",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Mumbai suburbs experience second highest 24-hr Sept rain in 26 years",
-        "links": "https:\/\/www.hindustantimes.com\/mumbai-news\/mumbai-suburbs-experience-second-highest-24-hr-sept-rain-in-26-yrs\/story-QmRciWGBKykNTpDwfSFVJN.html",
-        "text": "Mumbai suburbs experience second highest 24-hr Sept rain in 26 years",
-        "Paper_Name": "The_HT",
-        "Story_type": "Main_Story"
-    },
-    {
-        "headline": "Ladakh standoff | India, China agree to stop sending more troops to frontline ",
-        "links": "https:\/\/www.thehindu.com\/news\/national\/ladakh-standoff-new-army-talks-positive-but-no-breakthrough-on-de-escalation\/article32667313.ece",
-        "text": "Ladakh standoff | India, China agree to stop sending more troops to frontline ",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Farm Bills | Amid protests, kharif crops ripen",
-        "links": "https:\/\/www.thehindu.com\/sci-tech\/agriculture\/early-impact-of-farm-bills-likely-as-kharif-marketing-season-begins\/article32671878.ece",
-        "text": "Farm Bills | Amid protests, kharif crops ripen",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "PM Modi to review COVID-19 status in 7 States\/UTs today",
-        "links": "https:\/\/www.thehindu.com\/news\/national\/pm-modi-to-review-covid-19-status-in-7-high-burden-states-on-sept-23\/article32667413.ece",
-        "text": "PM Modi to review COVID-19 status in 7 States\/UTs today",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "COVID-19 | Festival crowds could pose a threat, warns NITI Aayog member",
-        "links": "https:\/\/www.thehindu.com\/news\/national\/niti-aayog-member-warns-of-infection-spread-during-festival-season-crowding\/article32670725.ece",
-        "text": "COVID-19 | Festival crowds could pose a threat, warns NITI Aayog member",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": "Hold China accountable for pandemic, Donald Trump tells UN ",
-        "links": "https:\/\/www.thehindu.com\/news\/international\/hold-china-accountable-for-pandemic-donald-trump-tells-un\/article32671485.ece",
-        "text": "Hold China accountable for pandemic, Donald Trump tells UN ",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Top_Stories"
-    },
-    {
-        "headline": " Parliament proceedings live updates | Rajya Sabha proceedings begin without Opposition parties",
-        "links": "https:\/\/www.thehindu.com\/news\/national\/parliament-proceedings-live-updates-september-23-2020\/article32673997.ece",
-        "text": " Parliament proceedings live updates | Rajya Sabha proceedings begin without Opposition parties",
-        "Paper_Name": "The_Hindu",
-        "Story_type": "Main_Story"
-    }
- ];
+{
+    "Indian_Express": [
+        {
+            "headline": "FinCEN Files | Antiques smuggler in TN jail, and a trade that flourished even after his arrest",
+            "links": "https://indianexpress.com/article/express-exclusive/fincen-filesremove-antiques-smuggler-in-tamil-nadu-jail-and-a-trade-that-flourished-even-after-his-arrest-6606764/"
+        },
+        {
+            "headline": "India, China agree no more escalation, to see if it holds",
+            "links": "https://indianexpress.com/article/india/india-china-agree-no-more-escalation-to-see-if-it-holds-6606802/"
+        },
+        {
+            "headline": "As Sept surge sweeps rural Maharashtra, hunt for beds",
+            "links": "https://indianexpress.com/article/india/as-sept-surge-sweeps-rural-maharashtra-hunt-for-beds-6606828/"
+        },
+        {
+            "headline": "Explained: How remunerative is farming at present?",
+            "links": "https://indianexpress.com/article/explained/simply-put-the-farmer-a-field-report-6606785/"
+        },
+        {
+            "headline": "Ravi Shankar Prasad: \u00e2\u20ac\u02dcOur commitment to farmers remains, Opposition will be exposed\u00e2\u20ac\u2122",
+            "links": "https://indianexpress.com/article/india/ravi-shankar-prasad-our-commitment-to-farmers-remains-once-people-realise-it-opposition-will-be-exposed-6606797/"
+        },
+        {
+            "headline": "Cyprus to Isle of Man: Over 100 transactions linked to Max chairman",
+            "links": "https://indianexpress.com/article/express-exclusive/fincen-files-cyprus-to-isle-of-man-over-100-transactions-linked-to-max-chairman-6606773/"
+        }
+    ],
+    "The_HT": [
+        {
+            "headline": "Mumbai Rains LIVE: City municipal commissioner asks citizens to remain indoors",
+            "links": "https://liveupdates.hindustantimes.com/india/mumbai-rains-live-updates-western-railway-suspends-services-due-to-waterlogging-21600829580738.html"
+        },
+        {
+            "headline": "Landslide warning in north Bengal including Darjeeling and Kalimpong",
+            "links": "https://www.hindustantimes.com/kolkata/landslide-warning-in-north-bengal-including-darjeeling-and-kalimpong/story-hPqeXjra6v1PEYIvwSGfQL.html"
+        },
+        {
+            "headline": "Would Tendulkar score a ton without spectators? Laxman gives fitting reply",
+            "links": "https://www.hindustantimes.com/cricket/would-sachin-tendulkar-score-a-ton-without-spectators-vvs-laxman-gives-fitting-reply/story-XEqAH4wmULiKWKxEHYk9zO.html"
+        },
+        {
+            "headline": "IPL 2020: Dhoni explains why he batted at No. 7 for CSK against RR",
+            "links": "https://www.hindustantimes.com/cricket/ipl-2020-ms-dhoni-explains-the-reason-behind-batting-at-no-7-for-csk-against-rr/story-FBDhclFh3mHEGjkwt4AhTM.html"
+        },
+        {
+            "headline": "Covid-19 meet with PM: Maharashtra may raise issue of lack of Centre\u00e2\u20ac\u2122s support ",
+            "links": "https://www.hindustantimes.com/cities/covid-19-meet-with-pm-maharashtra-may-raise-issue-of-lack-of-centre-s-support/story-yNIEj4ktfbdBbWBwaTPWuM.html"
+        },
+        {
+            "headline": "Parliament monsoon session: What to expect today",
+            "links": "https://www.hindustantimes.com/india-news/parliament-monsoon-session-what-to-expect-today/story-KpkkKk22G45TZ8uv9y9XjJ.html"
+        },
+        {
+            "headline": "PM Modi to meet chief ministers of  7 states. Here\u00e2\u20ac\u2122s what to expect",
+            "links": "https://www.hindustantimes.com/india-news/pm-modi-to-meet-chief-ministers-of-these-7-states-here-s-why/story-bEOs2kOg8RCoxtFkn1aD8H.html"
+        },
+        {
+            "headline": "Local trains cancelled in Mumbai due to heavy rainfall and waterlogging",
+            "links": "https://www.hindustantimes.com/mumbai-news/local-trains-cancelled-in-mumbai-due-to-heavy-rainfall-and-waterlogging/story-KN0BLtSp6R2f6609Fx8EwL.html"
+        },
+        {
+            "headline": "Mumbai suburbs experience second highest 24-hr Sept rain in 26 years",
+            "links": "https://www.hindustantimes.com/mumbai-news/mumbai-suburbs-experience-second-highest-24-hr-sept-rain-in-26-yrs/story-QmRciWGBKykNTpDwfSFVJN.html"
+        }
+    ],
+    "The_Hindu": [
+        {
+            "headline": "Ladakh standoff | India, China agree to stop sending more troops to frontline ",
+            "links": "https://www.thehindu.com/news/national/ladakh-standoff-new-army-talks-positive-but-no-breakthrough-on-de-escalation/article32667313.ece"
+        },
+        {
+            "headline": "Farm Bills | Amid protests, kharif crops ripen",
+            "links": "https://www.thehindu.com/sci-tech/agriculture/early-impact-of-farm-bills-likely-as-kharif-marketing-season-begins/article32671878.ece"
+        },
+        {
+            "headline": "PM Modi to review COVID-19 status in 7 States/UTs today",
+            "links": "https://www.thehindu.com/news/national/pm-modi-to-review-covid-19-status-in-7-high-burden-states-on-sept-23/article32667413.ece"
+        },
+        {
+            "headline": "COVID-19 | Festival crowds could pose a threat, warns NITI Aayog member",
+            "links": "https://www.thehindu.com/news/national/niti-aayog-member-warns-of-infection-spread-during-festival-season-crowding/article32670725.ece"
+        },
+        {
+            "headline": "Hold China accountable for pandemic, Donald Trump tells UN ",
+            "links": "https://www.thehindu.com/news/international/hold-china-accountable-for-pandemic-donald-trump-tells-un/article32671485.ece"
+        },
+        {
+            "headline": " Parliament proceedings live updates | Rajya Sabha proceedings begin without Opposition parties",
+            "links": "https://www.thehindu.com/news/national/parliament-proceedings-live-updates-september-23-2020/article32673997.ece"
+        }
+    ]
+}
